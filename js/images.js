f543a8ec3859bfcbfcec9fdb42634fa39cd9d95c
samiscold <62956581+samiscold@users.noreply.github.com>
2020-12-20 06:07:46 +0100

    Revert "New branch"

--- js/images.js
+++ js/images.js
@@ -1,65 +1,27 @@
+var i = 0;
+var images = [];
+var time = 8000;
 
 
-var i = 0; 			// Start Point
-var images = [];	// Images Array
-var time = 8000;	// Time Between Switch
-	 
-// Image List
-images[0] = "img/ubt-studentat.jpg";
-images[1] = "img/ubt-campus.jpg";
-images[2] = "img/ubt-slide2.jpg";
-images[3] = "img/ubt-slide-4.jpg";
-
-// Change Image
-function changeImg(){
-	document.slide.src = images[i];
+images[0] =  'img/ubt-campus.jpg';
+images[1] =  'img/ubt-slide2.jpg';
+images[2] =  'img/ubt-slide-4.jpg';
 
 
-	// Check If Index Is Under Max
-	if(i < images.length - 1){
-	  // Add 1 to Index
-      i++; 
+function changeImg() {
+    document.slide.src = images[i];
 
-	} else { 
-        // Reset Back To O
-  
-		i = 0;
-	}
+    if(i < images.length - 1) {
+        i++
+    } else {
+        i = 0;
 
-    
-	// Run function every x seconds
-	setTimeout("changeImg()", time);
+    }
+
+
+
+    setTimeout("changeImg()", time);
 }
 
-// function stop() {
+window.onload = changeImg;
 
-//     var counter = 0;
-
-//     if (i > 0) {
-    
-//     document.slide.src = images[--i];
-//         time = 999999;
-//         counter ++;
-//     }
-
-
-
-    
-
-//     if(counter == 1 ) {
-//         time = 0;
-//     }
-
-//     if(counter > 1 ) {
-//         time = 2000;
-//     }
-
-//     document.slide.src = images[i];
-//     time = 999999;
-    
-// }
-
-
-
-// Run function when page loads
-window.onload=changeImg;
